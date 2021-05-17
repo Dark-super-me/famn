112397aad827d2ce4db1153c53d42bd8466ca980
K41111NUX <79648002+Senpai-sama-afk@users.noreply.github.com>
2021-05-17 16:07:36 +0400

    Update stuff.py

--- helper/stuff.py
+++ helper/stuff.py
@@ -71,8 +71,8 @@ async def sencc(e):
         "Choose Mode",
         buttons=[
             [
-                Button.inline("Default Compress", data=f"encc{key}"),
-                Button.inline("Custom Compress", data=f"ccom{key}"),
+                Button.inline("Dual Audio Compress", data=f"encc{key}"),
+                Button.inline("Single Audio Compress", data=f"sao{key}"),
             ],
             [Button.inline("Back", data=f"back{key}")],
         ],
@@ -92,36 +92,3 @@ async def back(e):
         ],
     )
 
-
-async def ccom(e):
-    await e.edit("Send Ur Custom Name For That File")
-    wah = e.pattern_match.group(1).decode("UTF-8")
-    wh = decode(wah)
-    out, dl, thum, dtime = wh.split(";")
-    chat = e.sender_id
-    async with e.client.conversation(chat) as cv:
-        reply = cv.wait_event(events.NewMessage(from_users=chat))
-        repl = await reply
-        if "." in repl.text:
-            q = repl.text.split(".")[-1]
-            g = repl.text.replace(q, "mkv")
-        else:
-            g = repl.text + ".mkv"
-        outt = f"encode/{chat}/{g}"
-        x = await repl.reply(
-            f"Custom File Name : {g}\n\nSend Thumbnail Picture For it."
-        )
-        replyy = cv.wait_event(events.NewMessage(from_users=chat))
-        rep = await replyy
-        if rep.media:
-            tb = await e.client.download_media(rep.media, f"thumb/{chat}.jpg")
-        elif rep.text and not (rep.text).startswith("/"):
-            url = rep.text
-            os.system(f"wget {url}")
-            tb = url.replace("https://telegra.ph/file/", "")
-        else:
-            tb = thum
-        omk = await rep.reply(f"Thumbnail {tb} Setted Successfully")
-        hehe = f"{outt};{dl};{tb};{dtime}"
-        key = code(hehe)
-        await customenc(omk, key)
