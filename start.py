e240484ea0a6bcfd46ece291d088f6fb54a6f4d0
K41111NUX <79648002+Senpai-sama-afk@users.noreply.github.com>
2021-06-16 19:29:29 +0400

    Update start.py

--- start.py
+++ start.py
@@ -17,9 +17,9 @@ from helper._get import *
 
 APP_ID = 3635553
 API_HASH = "8d5b6c1a43d2d6169bd99aad0fc35701"
-BOT_TOKEN = "1612895957:AAHrZmaYWx8G4HcDyAUobAzKQQJOXRIT0Yo"
-OWNER_ID = -1001367162835
-LOG = 1760568371
+BOT_TOKEN = "1881783399:AAHX06IevCxyFzPw_d9l6f9yxoYr9MUw2QQ"
+OWNER = 1391975600
+LOG = 
 
 basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
 LOGS = getLogger(__name__)
