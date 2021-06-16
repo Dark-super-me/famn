cc846041d13417faeab064d76ac2973362f1ac23
K41111NUX <79648002+Senpai-sama-afk@users.noreply.github.com>
2021-06-16 20:10:40 +0400

    Update start.py

--- start.py
+++ start.py
@@ -19,7 +19,7 @@ APP_ID = 3635553
 API_HASH = "8d5b6c1a43d2d6169bd99aad0fc35701"
 BOT_TOKEN = "1881783399:AAHX06IevCxyFzPw_d9l6f9yxoYr9MUw2QQ"
 OWNER = 1391975600
-LOG = 
+LOG = -1001472251228
 
 basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
 LOGS = getLogger(__name__)
